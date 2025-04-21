b4d0813e344a57a9465ba894dcc6bbc4d8596cc0
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-21 08:46:40 +0300

    change the flow to required path

--- wallpaper.py
+++ wallpaper.py
@@ -5,15 +5,10 @@ import sys
 from random import choice 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-P','--path',help="directory background's all path")
+parser.add_argument('-P','--path',help="directory background's all path", required=True)
+parser.add_argument('-T','--time',type=float,help="time in seconde that you need to change your background")
 args = parser.parse_args()
 path = args.path 
-
-if not path:
-    os.system('echo argument required')
-    os.system('python3 wallpaper.py --help')
-    sys.exit()
-
 data = os.listdir(path)
 
 wallpapers = list(map(lambda n : f"file://{path}/{n}",data))
@@ -21,7 +16,6 @@ wallpapers = list(map(lambda n : f"file://{path}/{n}",data))
 while True:
     wallpaper = choice(wallpapers)
     os.system(f'gsettings set org.gnome.desktop.background picture-uri-dark {wallpaper}')
-    print(wallpaper)
     time.sleep(20.0)
 
 
