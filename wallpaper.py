e197b7c4fce2e5d70ecc71d8cc95e70559a7205c
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-21 08:42:00 +0300

    change sequence to a random

--- wallpaper.py
+++ wallpaper.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import time 
 import sys 
+from random import choice 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-P','--path',help="directory background's all path")
@@ -17,14 +18,10 @@ data = os.listdir(path)
 
 wallpapers = list(map(lambda n : f"file://{path}/{n}",data))
 
-
-i = 0 
-
-while i<len(wallpapers):
-    os.system(f'gsettings set org.gnome.desktop.background picture-uri-dark {wallpapers[i]}')
-    print(wallpapers[i])
-    i+=1
-    if i==len(wallpapers):i=0
+while True:
+    wallpaper = choice(wallpapers)
+    os.system(f'gsettings set org.gnome.desktop.background picture-uri-dark {wallpaper}')
+    print(wallpaper)
     time.sleep(20.0)
 
 
