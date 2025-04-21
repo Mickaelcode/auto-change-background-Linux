97a595a29d8061da1ccbff6b3013ddf38623e878
Mickael Joseph <mickaelandriana06@gmail.com>
2025-04-21 08:33:55 +0300

    fix bigs path

--- wallpaper.py
+++ wallpaper.py
@@ -5,17 +5,17 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-P','--path',help="directory background's path")
-
 args = parser.parse_args()
+
 if not args.path:
     os.system('echo argument required')
     os.system('python3 wallpaper.py --help')
     sys.exit()    
-print(os.path.abspath(args.path))
-wallpapers_path = f'~/{args.path}'
-data = os.listdir(os.path.expanduser(wallpapers_path))
 
-wallpapers = list(map(lambda n : f"file://{wallpapers_path}/{n}",data))
+user = os.getlogin()
+data = os.listdir(os.path.expanduser(f"~/{args.path}"))
+
+wallpapers = list(map(lambda n : f"file:///home/{user}/{args.path}/{n}",data))
 
 
 i = 0 
